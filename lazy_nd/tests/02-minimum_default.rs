70e07e1b1f96129775890ce9c287b63ffac45e2e
JWSchaefer <joe.w.schaefer@gmail.com>
2025-01-24 00:54:38 +0000

    Split lazy-nd into standalone crate.

--- lazy_nd/tests/02-minimum_default.rs
+++
@@ -1,4 +0,0 @@
-use lazy_nd::lazy_nd;
-#[lazy_nd(dim = D)]
-struct Test<const D: usize = 2> {}
-fn main() {}
--- lazy_nd/tests/progress.rs
+++
@@ -1,6 +0,0 @@
-#[test]
-fn tests() {
-    let t = trybuild::TestCases::new();
-    t.pass("tests/01-minimum.rs");
-    t.pass("tests/02-minimum_default.rs");
-}
